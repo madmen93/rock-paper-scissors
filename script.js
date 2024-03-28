503d60fc797a6009549d85fbf86fb3aeaf51beef
Monica Daza <158379665+madmen93@users.noreply.github.com>
2024-03-28 15:15:08 -0500

    Crear función getComputerSelection

--- script.js
+++ script.js
@@ -13,8 +13,24 @@ let number = Math.floor(Math.random()*(3 - 1)+1);
 return number;
 }
 
+let selection = getRandomNumber();
 
-console.log(getRandomNumber());
+function getComputerChoice(selection){
+    let sel;
+    switch (selection) {
+        case 1:
+            sel = "roca";
+            break;
+        case 2:
+            sel = "papel";
+            break;
+        case 3:
+            sel = "tijeras";
+            break;
+    }
+    return sel;
+}
+console.log(getComputerChoice(selection));
 //Resultado de la ronda e impresión del resultado de la ronda
 
 //Resultado del juego e impresión del resultado del juego
