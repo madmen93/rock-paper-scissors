75e72ce5467267962e1548c0d0f37431536d0df1
Monica Daza <158379665+madmen93@users.noreply.github.com>
2024-03-28 14:38:51 -0500

    Crear la variable playerSelection

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 console.log("Hello world");
 
 //Determinar valores iniciales
-const paper = "paper";
-const rock = "rock";
-const sicssors = "rock";
+const paper = "papel";
+const rock = "roca";
+const sicssors = "tijeras";
 
 //Obtener la jugada del usuario
+let playerSelection = prompt("Ingrese roca, papel o tijeras:");
 //Obtener la jugada de la computadora
+
 //Resultado de la ronda e impresión del resultado de la ronda
+
 //Resultado del juego e impresión del resultado del juego
