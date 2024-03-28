d417968c4669ead4df182f548db1f03068ffeeaf
Monica Daza <158379665+madmen93@users.noreply.github.com>
2024-03-28 14:33:40 -0500

    Crear variables principales en script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Hello world");
+
+//Determinar valores iniciales
+const paper = "paper";
+const rock = "rock";
+const sicssors = "rock";
+
+//Obtener la jugada del usuario
+//Obtener la jugada de la computadora
+//Resultado de la ronda e impresión del resultado de la ronda
+//Resultado del juego e impresión del resultado del juego
