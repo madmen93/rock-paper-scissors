9378a7aeab62520bb604445c3999543dfffefd6a
Monica Daza <158379665+madmen93@users.noreply.github.com>
2024-03-28 15:03:11 -0500

    Crear una función para obtener un número aleatorio entre 1 y 3

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-console.log("Hello world");
 
 //Determinar valores iniciales
 const paper = "papel";
@@ -7,8 +6,15 @@ const sicssors = "tijeras";
 
 //Obtener la jugada del usuario
 let playerSelection = prompt("Ingrese roca, papel o tijeras:");
-//Obtener la jugada de la computadora
 
+//Obtener la jugada de la computadora
+function getRandomNumber(){
+let number = Math.floor(Math.random()*(3 - 1)+1);
+return number;
+}
+
+
+console.log(getRandomNumber());
 //Resultado de la ronda e impresión del resultado de la ronda
 
 //Resultado del juego e impresión del resultado del juego
